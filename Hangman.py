5cf13d5c84c65c209e6e09118b3f25a5bbb82684
K-Coder404 <hartingerk@posteo.de>
2024-01-26 11:23:16 +0100

    Added words list and visuals

--- Hangman.py
+++ Hangman.py
@@ -1,21 +1,55 @@
-"""Dieses Programm soll ein Hangman-Spiel werden
-Im Prinzip braucht man: 
--Eine Liste aus Wörtern, die es zu erraten gilt
--Ein Wort aus der Liste wird random ausgesucht und dann die Anzahl der Buchstaben als _ dargestellt
--Rateversuche
--Überprüfung, ob der Buchstabe im Wort enthalten ist
--An welcher Stelle enthalten ist
--Wenn RICHTIG --> An der gleichen Stelle (innerhalb des Wortes) eingesetzt (_s_el)
--Wenn FLASCH --> Versuch +1 und Zeichnung des Galgen"""
+#importiert von words.py die liste word_list
+from words import word_list
+#importiert von visual.py die Visuelle Liste der Tries von Hangman
+from visual import display_hangman 
 
 import random
 
-rateliste = ["Baum", "Affe", "Hangman", "Schneemann"]
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
 
-zufalls_begriff = random.choice(rateliste)
-verbergen_zb = ''.join(['-' for _ in zufalls_begriff])
+def play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print("Let's Play Hangman!")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or word:").upper()
+### Die Methode isalpha() ist eine in Python eingebaute Zeichenkettenmethode (String-Methode), 
+### die dazu verwendet wird, zu überprüfen, ob alle Zeichen in einer Zeichenkette alphabetische Zeichen sind
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Good job", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" == word_completion:
+                    guessed = True
+                
+            
+        elif len(guess) == len(word) and guess.isalpha():
 
-print(zufalls_begriff)
-print(verbergen_zb)
+        else:
+            print("Not a valid guess")
+        print(display_hangman(tries))    
+        print(word_completion)
+        print("\n")
+
+
 
 
