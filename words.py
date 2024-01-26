92e692f5e4c92ccafcea9c10b6fc6e3a1bc9079b
K-Coder404 <hartingerk@posteo.de>
2024-01-26 11:28:31 +0100

    finally added visuals and wordlist

---
+++ words.py
@@ -0,0 +1,12 @@
+
+word_list = ['apple', 'banana', 'orange', 'grape', 'kiwi', 'pineapple', 'strawberry', 'blueberry', 'melon', 'watermelon',
+             'peach', 'pear', 'plum', 'cherry', 'mango', 'apricot', 'lemon', 'lime', 'avocado', 'coconut',
+             'peanut', 'almond', 'walnut', 'cashew', 'pecan', 'hazelnut', 'pistachio', 'macadamia', 'chestnut',
+             'carrot', 'broccoli', 'cucumber', 'potato', 'tomato', 'onion', 'garlic', 'pepper', 'lettuce', 'spinach',
+             'zucchini', 'eggplant', 'radish', 'cabbage', 'cauliflower', 'asparagus', 'celery', 'cantaloupe', 'fig',
+             'python', 'programming', 'language', 'computer', 'algorithm', 'variable', 'function', 'loop', 'condition',
+             'database', 'network', 'web', 'internet', 'software', 'hardware', 'interface', 'object', 'class', 'method',
+             'module', 'library', 'framework', 'version', 'code', 'debug', 'error', 'exception', 'syntax', 'semantic',
+             'logic', 'bug', 'iteration', 'index', 'pointer', 'variable', 'assignment', 'operator', 'expression',
+             'data', 'structure', 'list', 'tuple', 'dictionary', 'set', 'file', 'stream', 'input', 'output', 'buffer']
+
